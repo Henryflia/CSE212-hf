8d7ea5d1820d7d2e1c65026702c3a7667fa84015
henryflia <superflia2059@gmail.com>
2026-01-13 22:47:13 -0400

    W1 Dinamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,17 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // TODO Problem 1 Start
+        // Step 1: I need to create and array of number with the length
+        double[] numbers = new double[length];
+
+        // Step 2: Create a for loop that iterates through the array and multiplies the numbers.
+        for (int i = 0; i < length; i++)
+        {
+            numbers[i] = number * (i + 1);
+        }
+        return numbers; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +34,15 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        // Step 1 Divide The list woth the Amount in two variables
+        List<int> number1 = data.GetRange(data.Count - amount, amount);
+        List<int> number2 = data.GetRange(0, data.Count - amount);
+
+        // Step 2 Clear the data List
+        data.Clear();
+
+        // Step 3 Add the two List Variables to the Data List in the correct order
+        data.AddRange(number1);
+        data.AddRange(number2);
     }
 }
